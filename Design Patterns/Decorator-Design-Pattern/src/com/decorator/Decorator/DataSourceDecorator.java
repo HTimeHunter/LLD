0ccc85d216c72b9a72880c85f584d0f774f6c5fb
harshkathiriya3112 <harshdkathiriya@gmail.com>
2024-12-20 17:08:33 +0530

    added decorator pattern

---
+++ Design Patterns/Decorator-Design-Pattern/src/com/decorator/Decorator/DataSourceDecorator.java
@@ -0,0 +1,21 @@
+package com.decorator.Decorator;
+
+import com.decorator.DataSource;
+
+public abstract class DataSourceDecorator implements DataSource {
+    private DataSource wrappee;
+
+    DataSourceDecorator(DataSource source) {
+        this.wrappee = source;
+    }
+
+    @Override
+    public void writeData(String data) {
+        wrappee.writeData(data);
+    }
+
+    @Override
+    public String readData() {
+        return wrappee.readData();
+    }
+}
